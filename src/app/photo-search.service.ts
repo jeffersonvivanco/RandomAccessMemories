f6bbe0e328abee9d6c6721903f3080f1aedbca7f
jeffersonvivanco <jv1019@nyu.edu>
2017-06-18 12:32:56 -0400

    fixed something

--- src/app/photo-search.service.ts
+++ src/app/photo-search.service.ts
@@ -6,7 +6,8 @@ import { Photo } from './photo';
 
 @Injectable()
 export class PhotoSearchService {
-  private photosUrl = 'http://localhost:3000/api/photos';
+  // private photosUrl = 'http://localhost:3000/api/photos';
+  private photosUrl = '/api/photos';
   constructor(private http: Http) {}
   search(term: string): Observable<Photo[]> {
     return this.http.get(`${this.photosUrl}/?name=${term}`).map(response => response.json() as Photo[]);
